ef79d088e655d3146c79ebe8033e7ee436b5878e
elpeAJ <elpediaarthurjunior@gmail.com>
2024-01-26 12:37:23 +0000

    main file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function changeColor(){
+  const colorChange = document.querySelector()
+  button.style()
+}
+
+const button = document.querySelector("#button");
+button.addEventListener("click", changeColor);
