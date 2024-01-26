7505b46b40aed2c442d781e41545977aa945aed2
elpeAJ <elpediaarthurjunior@gmail.com>
2024-01-26 15:59:28 +0000

    main file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-function changeColor(){
-  const colorChange = document.querySelector()
-  button.style()
+function changeColor() {
+  const colorChange = document.querySelector("#button");
+  colorChange.style.background = "blue"; // Example: change text color to blue
 }
 
+function hoverColor() {
+  const hoverChange = document.querySelector("#button");
+  hoverChange.style.background = "grey"; // Example: change text color to blue
+}
+
+function redColor() {
+  const redChange = document.querySelector("#button");
+  redChange.style.background = "red"; // Example: change text color to blue
+}
 const button = document.querySelector("#button");
-button.addEventListener("click", changeColor);
+// button.addEventListener("click", changeColor);
+button.addEventListener("mouseover", hoverColor);
+// button.addEventListener("mouseout", redColor);
